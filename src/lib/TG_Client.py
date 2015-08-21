a22acc69efc7c88788c3d50dd9c171b8aa48f174
wei bai <baiwei0427@sina.com>
2015-08-22 02:23:56 +0800

    Update TG_Client
    1. Update TG_Client.py
    2. Add a simple example program

--- src/lib/TG_Client.py
+++ src/lib/TG_Client.py
@@ -2,11 +2,22 @@ import socket
 import datetime
 import sys
 
+'''
+TG_Client is a python class to generate requests and measure flow completion times (FCT)
+ip: IP address of the remote server who actually generates flows
+port: TCP port of the remote server
+'''
 class TG_Client:
     def __init__(self, ip, port):
         self.ip=ip
         self.port=port
 
+'''
+size: flow size (KB)
+clientToS: ToS value of client socket (traffic from the client to the remote server)
+serverToS: ToS value of server socket (traffic from the remote server to the client)
+return FCT results (microseconds)
+'''
     def request(self, size, clientToS, serverToS):
         s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         if clientToS>0 and clientToS<=255:
@@ -25,7 +36,9 @@ class TG_Client:
         fct=endTime-startTime
         return int(fct.total_seconds()*1000000)
 
-
+'''
+A simple example
+'''
 if __name__=='__main__':
     if len(sys.argv)==6:
         client=TG_Client(sys.argv[1],int(sys.argv[2]))
