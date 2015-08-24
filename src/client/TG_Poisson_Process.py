b503a8c8c4572cde28d4a3aeee2f50ea6c6d7d25
wei bai <baiwei0427@sina.com>
2015-08-24 15:26:47 +0800

    Add multi-service support
    Different services can have different flow size distributions

--- src/client/TG_Poisson_Process.py
+++ src/client/TG_Poisson_Process.py
@@ -8,13 +8,17 @@ from TG_Distribution import TG_Distribution
 from TG_Client import TG_Client
 
 class TG_Poisson_Thread(threading.Thread):
-    def __init__(self, senders, dist, throughput, serviceNum):
+    def __init__(self, senders, dists, throughput, serviceNum):
         self.senders=senders    #sender list
-        self.dist=dist  #flow size distribution
-        self.avgReqRate=throughput*1024*1024/(self.dist.getAvgFlowSize()*1024*8) #average request rate (number per second)
+        self.dists=dists    #flow size distributions of different services
         self.services=serviceNum    #number of services
         self.client=TG_Client() #client socket
-        self.deficit=0 #deficit (us)
+        self.deficit=0  #deficit (us)
+        avgFlowSize=0
+        for dist in self.dists:
+            avgFlowSize=avgFlowSize+dist.getAvgFlowSize()*1024*8    #KB to bit
+        avgFlowSize=avgFlowSize/len(self.dists)
+        self.avgReqRate=throughput*1024*1024/avgFlowSize    #average request rate (number per second)
         threading.Thread.__init__(self)
 
     '''
@@ -29,18 +33,23 @@ class TG_Poisson_Thread(threading.Thread):
             '''We have generated enough flows'''
             if flows>=flowNum:
                 break
+
+            '''update flows'''
             mutex.acquire()
             flows=flows+1
             mutex.release()
+
             '''Generate request and measure FCT'''
             startTime=datetime.datetime.now()
             sender=self.senders[random.randint(0,len(self.senders)-1)]
-            size=self.dist.genFlowSize()
-            tos=random.randint(0,self.services-1)*4    #client and server socket share the same ToS value
+            serviceID=random.randint(0,self.services-1) #randomly choose a service
+            tos=serviceID*4 #client and server socket share the same ToS value
+            size=self.dists[serviceID].genFlowSize()    #generate flow size according to distribution of this service
             fct=self.client.request(sender, 5001, size, tos, tos)
-            FCTs.append([size, fct])
+            FCTs.append([size, fct, serviceID]) #python list should be thread-safe
             endTime=datetime.datetime.now()
             expireTime=(endTime-startTime).total_seconds()*1000000  #expire time (us)
+
             sleepTime=self.nextTime()-expireTime-self.deficit
             if sleepTime>0:
                 self.deficit=0
@@ -50,22 +59,26 @@ class TG_Poisson_Thread(threading.Thread):
 
 
 if __name__=='__main__':
-    if len(sys.argv)==8:
+    if len(sys.argv)<8:
+        print 'Usage '+sys.argv[0]+' [senders file] [average throughput (Mbps)] [number of threads] [number of flows] [result file] [number of services (N)] [flow size CDF of service 1] ...[flow size CDF of service N]'
+    else:
         senderFileName=sys.argv[1]
-        cdfFileName=sys.argv[2]
-        throughput=int(sys.argv[3])
-        serviceNum=int(sys.argv[4])
-        threadNum=int(sys.argv[5])
-        flowNum=int(sys.argv[6])
-        resultFileName=sys.argv[7]
+        throughput=int(sys.argv[2])
+        threadNum=int(sys.argv[3])
+        flowNum=int(sys.argv[4])
+        resultFileName=sys.argv[5]
+        serviceNum=int(sys.argv[6])
+        cdfFileNames=sys.argv[7:]
 
         '''Get destinations'''
         senderFile=open(senderFileName)
         senders=senderFile.readlines()
         senderFile.close()
 
-        '''Get distribution'''
-        dist=TG_Distribution(cdfFileName)
+        '''Get distribution of different services'''
+        dists=[]
+        for f in cdfFileNames:
+            dists.append(TG_Distribution(f))
 
         '''Number of flows that have been generated'''
         flows=0
@@ -77,7 +90,7 @@ if __name__=='__main__':
         '''Start threads'''
         threads=[]
         for i in range(threadNum):
-            t=TG_Poisson_Thread(senders, dist, throughput/threadNum, serviceNum)
+            t=TG_Poisson_Thread(senders, dists, throughput/threadNum, serviceNum)
             threads.append(t)
             t.start()
 
@@ -88,8 +101,8 @@ if __name__=='__main__':
         '''Write FCT results to the file'''
         resultFile=open(resultFileName,'w')
         for result in FCTs:
-            resultFile.write(str(result[0])+' '+str(result[1])+'\n')
+            if len(result)==3:
+                resultFile.write(str(result[0])+' '+str(result[1])+' '+str(result[2])+'\n')
+            else:
+                print 'result error'
         resultFile.close()
-
-    else:
-        print 'Usage '+sys.argv[0]+' [sender file] [flow size CDF file] [average throughput (Mbps)] [number of services] [number of threads] [number of flows] [result file]'
