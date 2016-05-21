93a510bd9fe0fc1178a848b7d1d0bcbc24031223
wei bai <baiwei0427@sina.com>
2016-05-21 17:58:12 +0800

    Remove some functions & Support path name matching

--- src/script/result.py
+++ src/script/result.py
@@ -2,93 +2,84 @@ import sys
 import os
 import glob
 
-def fct_result_file(fileName):
-    f = open(fileName)
+''' Parse a file to get FCT and goodput results '''
+def parse_file(file_name):
     results = []
+    f = open(file_name)
     while True:
         line = f.readline().rstrip()
         if not line:
             break
         arr = line.split()
-        if len(arr) >= 2:
-            '''size, fct, dscp, sending rate, goodput'''
-            results.append([int(arr[0]), int(arr[1])])
-    f.close()
-    return results
-
-def goodput_result_file(fileName):
-    f = open(fileName)
-    results = []
-    while True:
-        line = f.readline().rstrip()
-        if not line:
-            break
-        arr = line.split()
+        '''size, fct, dscp, sending rate, goodput'''
         if len(arr) >= 5:
-            '''size, fct, dscp, sending rate, goodput'''
-            results.append([int(arr[0]), int(arr[4])])
+            '''[size, fct, goodput]'''
+            results.append([int(arr[0]), int(arr[1]), int(arr[4])])
     f.close()
     return results
 
-def fct_result_dir(dirName):
-    results=[]
-    for f in glob.glob(dirName+'*.txt'):
-        results.extend(fct_result_file(f))
-    return results
-
-def goodput_result_dir(dirName):
-    results=[]
-    for f in glob.glob(dirName+'*.txt'):
-        results.extend(goodput_result_file(f))
-    return results
-
-def average_result(input_tuple_list):
-    input_list = [x[1] for x in input_tuple_list]
+''' Get average result '''
+def average_result(input_tuple_list, index):
+    input_list = [x[index] for x in input_tuple_list]
     if len(input_list) > 0:
         return sum(input_list) / len(input_list)
     else:
         return 0
 
-def tail_result(input_tuple_list):
-    input_list = [x[1] for x in input_tuple_list]
+''' Get 99th percentile result '''
+def tail_result(input_tuple_list, index):
+    input_list = [x[index] for x in input_tuple_list]
     input_list.sort()
     if len(input_list) > 0:
         return input_list[int(0.99 * len(input_list))]
     else:
         return 0
 
-def print_fct_results(results):
+def average_fct_result(input_tuple_list):
+    return average_result(input_tuple_list, 1)
+
+def average_goodput_result(input_tuple_list):
+    return average_result(input_tuple_list, 2)
+
+def tail_fct_result(input_tuple_list):
+    return tail_result(input_tuple_list, 1)
+
+def tail_goodput_result(input_tuple_list):
+    return tail_result(input_tuple_list, 2)
+
+
+def print_result(results):
      # (0, 100KB)
-    small = filter(lambda x: x[0] < 100*1024, results)
+    small = filter(lambda x: x[0] < 100 * 1024, results)
     # (100KB, 10MB)
-    medium = filter(lambda x: 100*1024 <= x[0] < 10240*1024, results)
+    medium = filter(lambda x: 100 * 1024 <= x[0] < 10 * 1024 * 1024, results)
     # (10MB, infi)
-    large = filter(lambda x: x[0] >= 10240*1024, results)
+    large = filter(lambda x: x[0] >= 10 * 1024 * 1024, results)
 
-    print '%d flows/requests overall average completion time: %d us' % (len(results), average_result(results))
-    print '%d flows/requests (0, 100KB) average completion time: %d us' % (len(small), average_result(small))
-    print '%d flows/requests (0, 100KB) 99th percentile completion time: %d us' % (len(small), tail_result(small))
-    print '%d flows/requests [100KB, 10MB) average completion time: %d us' % (len(medium), average_result(medium))
-    print '%d flows/requests [10MB, ) average completion time: %d us' % (len(large), average_result(large))
+    print '%d flows/requests overall average completion time: %d us' % (len(results), average_fct_result(results))
+    print '%d flows/requests (0, 100KB) average completion time: %d us' % (len(small), average_fct_result(small))
+    print '%d flows/requests (0, 100KB) 99th percentile completion time: %d us' % (len(small), tail_fct_result(small))
+    print '%d flows/requests [100KB, 10MB) average completion time: %d us' % (len(medium), average_fct_result(medium))
+    print '%d flows/requests [10MB, ) average completion time: %d us' % (len(large), average_fct_result(large))
+    print '%d flows/requests overall average goodput: %d Mbps' % (len(results), average_goodput_result(results))
 
-def print_goodput_results(results):
-    print '%d flows/requests overall average goodput: %d Mbps' % (len(results), average_result(results))
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
-        print 'Usages: %s [result file/directory]' % sys.argv[0]
         sys.exit()
 
-    path = sys.argv[1]
-    if os.path.isfile(path):
-        print_fct_results(fct_result_file(path))
-        #print '======================================================'
-        print_goodput_results(goodput_result_file(path))
-    elif os.path.isdir(path):
-        if not path.endswith('/'):
-            path = path + '/'
-        print_fct_results(fct_result_dir(path))
-        #print '======================================================'
-        print_goodput_results(goodput_result_dir(path))
+    files = glob.glob(sys.argv[1])
+    final_results = []
+    num_file_parse = 0
+
+    for f in files:
+        if os.path.isfile(f):
+            final_results.extend(parse_file(f))
+            num_file_parse = num_file_parse + 1
+
+    if num_file_parse <= 1:
+        print "Parse %d file" % num_file_parse
     else:
-        print 'Illegal input path'
+        print "Parse %d files" % num_file_parse
+
+    print_result(final_results)
