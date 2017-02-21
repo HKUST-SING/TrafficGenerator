375ee7265d8c6f7e63cccb1bcaa2398299fc7b69
lingysjustinas@gmail.com <lingysjustinas@gmail.com>
2017-02-21 04:43:14 -0800

    Final version of updates.

--- src/common/cdf.c
+++ src/common/cdf.c
@@ -121,15 +121,16 @@ double rand_range(double min, double max)
 
 /* generate a random value based on CDF distribution */
 double gen_random_cdf(struct cdf_table *table)
-{
-    int i = 0;
-    double x;
-    /* printf("%f %f %f\n", x, table->min_cdf, table->max_cdf); */
+{   
 
     if (!table)
-        return 0;
+        return 0.0;
 
-    x = rand_range(table->min_cdf, table->max_cdf);
+
+    int i = 0;
+    double x = rand_range(table->min_cdf, table->max_cdf);
+    /* printf("%f %f %f\n", x, table->min_cdf, table->max_cdf); */
+
 
     for (i = 0; i < table->num_entry; i++)
     {
